f321781e47447cffade30841756b776374a58ec4
David Stolarsky <david.stolarsky@gmail.com>
2010-01-19 21:48:38 +0000

    transform enabled supershapes.  they are pretty awesome.

--- Supershape Project/Assets/Plugins/Supershape.cs
+++ Supershape Project/Assets/Plugins/Supershape.cs
@@ -5,27 +5,56 @@ using System.Runtime.InteropServices;
 
 
 public class Supershape : MonoBehaviour {
-	// Unity component interface params
+    public enum TransformType { None, Logarithmic, Trigonometric, TransferFunction, Polynomial };
+
+    // Unity component interface params
 	public float a_m = 1f;
-    public double a_a = 1; 
-    public double a_b = 1;
+    public float a_a = 1; 
+    public float a_b = 1;
 	public float a_n1 = 1f;
 	public float a_n2 = 1f;
 	public float a_n3 = 1f;
 	public float b_m = 1f;
-    public double b_a = 1; 
-    public double b_b = 1;
+    public float b_a = 1; 
+    public float b_b = 1;
 	public float b_n1 = 1f;
 	public float b_n2 = 1f;
 	public float b_n3 = 1f;
-	public float min_phi = (float) - Math.PI;
+	
+    public float min_phi = (float) - Math.PI;
 	public float max_phi = (float) Math.PI;
 	public float min_theta = (float) - Math.PI/2;
-	public float max_theta = (float) Math.PI/2;
-	public bool Enable_Spiral = false;
-	public int xSteps = 64;
-	public int ySteps = 64;
+    public float max_theta = (float)Math.PI / 2;
+
+    public TransformType transform1_type;
+    public float transform1_var1;
+    public float transform1_var2;
+    public float transform1_var3;
+    public float transform1_var4;
+
+    public TransformType transform2_type;
+    public float transform2_var1;
+    public float transform2_var2;
+    public float transform2_var3;
+    public float transform2_var4;
+
+    /*
+    public TransformType transform_type1;
+    public float transform1Var1;
+    public float transform1Var2;
+    public float transform1Var3;
+    public float transform1Var4;
+    
+    public TransformType transform_type2;
+    public float transform2Var1;
+    public float transform2Var2;
+    public float transform2Var3;
+    public float transform2Var4;
+     */
 	
+    public int xSteps = 64;
+	public int ySteps = 64;
+
 	// 
 	private Mesh m_SupershapeMesh;
 	private Vector3[] m_Vertices;
@@ -43,6 +72,8 @@ public class Supershape : MonoBehaviour {
 	
     void Start () {
 		
+
+
         // Create game object containing renderer
         //GameObject myGameObject = new GameObject();
         MeshFilter meshFilter = gameObject.AddComponent<MeshFilter>();
@@ -73,7 +104,7 @@ public class Supershape : MonoBehaviour {
         //m_Texture.SetPixels (m_Pixels, 0);
         //m_Texture.Apply ();
 		
-		//CalcSupershape();
+		CalcSupershape();
     }
 	
 	
@@ -83,7 +114,7 @@ public class Supershape : MonoBehaviour {
 	{
 		double r;
 		double t1, t2;
-		//double b_a = 1, b_b = 1;
+		//float b_a = 1, b_b = 1;
 
 		t1 = Math.Cos(b_m * phi / 4) / b_a;
 		t1 = Math.Abs(t1);
@@ -112,7 +143,7 @@ public class Supershape : MonoBehaviour {
 	{
 		double r;
 		double t1, t2;
-		//double a_a = 1, a_b = 1;
+		//float a_a = 1, a_b = 1;
 
 		Eval2D(phi, ref x, ref y);
 
@@ -125,8 +156,8 @@ public class Supershape : MonoBehaviour {
 		t2 = Math.Pow(t2, a_n3);
 
 		r = Math.Pow(t1 + t2, 1 / a_n1);
-		if(Enable_Spiral)
-			r *= Math.Log(phi);
+		//if(Enable_Spiral)
+		//	r *= Math.Log(phi);
 		
 		if (Math.Abs(r) == 0)
 		{
@@ -137,12 +168,47 @@ public class Supershape : MonoBehaviour {
 		else
 		{
 			r = 1 / r;
-			x *= (float)(r * Math.Cos(theta));
-			y *= (float)(r * Math.Cos(theta));
-			z = (float)(r * Math.Sin(theta));
+
+            // Apply the R transform!
+	        // Radial function
+	        double rRadial = r;
+            DoTransform(ref rRadial, phi, transform1_type, transform1_var1, transform1_var2, transform1_var3, transform1_var4);
+
+	        x *= (float) rRadial * (float) Math.Cos(theta);
+	        y *= (float) rRadial * (float) Math.Cos(theta);
+
+	        double rVertical = r;
+            DoTransform(ref rVertical, phi, transform2_type, transform2_var1, transform2_var2, transform2_var3, transform2_var4);
+	        
+	        z = (float) rVertical * (float) Math.Sin(theta);
+
+
 		}
+
 	}
 	
+    void DoTransform(ref double from, float phi, TransformType type, float var1, float var2, float var3, float var4)
+    {
+        switch (type)
+        {
+	        case TransformType.None:					// No Transform
+		        break;
+            case TransformType.Logarithmic:			// Logarithmic = Var1 * e^(Var2 * Theta)
+                from *= var1 * Math.Exp(var2 * phi);
+		        break;
+            case TransformType.Trigonometric:			// Trigonometric = Var1 * cos (Var2 * Theta) + Var3 * sin (Var4 * Theta)
+                from *= var1 * Math.Cos(var2 * phi) + var3 * Math.Sin(var4 * phi);
+		        break;
+            case TransformType.TransferFunction:		// Transfer Function  = (Var1 + Var2 * Theta)/(Var3 + Var4 * Theta)
+                from *= (var1 + (var2 * phi)) / (var3 + (var4 * phi));
+		        break;
+            case TransformType.Polynomial:			// Polynomial = Var1 + Var2 * Theta + Var3 * Theta^2 + Var4 * Theta^3 + ...
+                from *= var1 + (var2 * phi) + (var3 * Math.Pow(phi, 2.0f)) + (var4 * Math.Pow(phi, 3.0f));
+		        // TODO: add the reset of the formula
+		        break;
+        }
+    }
+
 	void CalcSupershape()
 	{
 		float phi = -(float)Math.PI;
